83880d5f4740841b01b6d837eda44940298ce397
Serd3sh <romarius543@mail.ru>
2025-04-21 22:53:58 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-
+using namespace std;
 // **Обратный порядок слов**: 
 // Напишите функцию, которая принимает строку в качестве входного параметра и возвращает строку, в которой слова записаны в обратном порядке./
 
-std::string join(std::string str, std::string word) {
-  std::string newStr = "";
-  if(str.length() == 0) newStr = word;
+string join(string str, string word) {
+  string newStr = "";
+  if (str.length() == 0) newStr = word;
   else newStr = word + ' ' + str;
   return newStr; 
 }
 
-std::string reverse(std::string str) {
-  std::string word = "", newStr = "";
-  for(int i = 0; i < str.length(); i++) {
+string reverse(string str) {
+  string word = "", newStr = "";
+  for(size_t i = 0; i < str.length(); i++) {
     if(str[i] == ' ') {
       newStr = join(newStr, word);
       word = "";
@@ -20,13 +20,15 @@ std::string reverse(std::string str) {
       word += str[i];
     }
   }
-  if(word.length() > 0) newStr = join(newStr, word); // Последнее слово
+  if (word.length() > 0) newStr = join(newStr, word); // Последнее слово
   return newStr;
 }
 
 int main() {
-  std::string str = "Some string for example!";
-  std::string newStr = reverse(str);
-  std::cout << "Result: " << newStr << std::endl;
+  string str;
+  cout << "Input text: ";
+  getline(cin, str);
+  string newStr = reverse(str);
+  cout << "Result: " << newStr << endl;
   return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,15 @@
-all:
-	g++ main.cpp -o main
+compiler=g++
+flags=-Wall
+executable=reverse
+
+$(executable): main.o
+	$(compiler) $(flags) main.o -o $(executable)
+	rm -f main.o
+
+main.o: main.cpp
+	$(compiler) $(flags) -c main.cpp
+
+clean:
+	rm -f $(executable) main.o
+
+

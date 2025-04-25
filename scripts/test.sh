2d62f6126d372dfe54f68ef422513ef44d6499f4
Serd3sh <romarius543@mail.ru>
2025-04-25 21:00:47 +0300

    pipeline fixes3

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 declare -A tests
-tests = (
-	["one two three four five six seven eight"] = "eight seven six five four three two one"
-	["Lorem ipsum dolor sit amet consectetur adipiscing elit"] = "elit adipiscing consectetur amet sit dolor ipsum Lorem"
-	["hello world"] = "world hello"
-	["hi"] = "hi"
+tests=(
+	["one two three four five six seven eight"]="eight seven six five four three two one"
+	["Lorem ipsum dolor sit amet consectetur adipiscing elit"]="elit adipiscing consectetur amet sit dolor ipsum Lorem"
+	["hello world"]="world hello"
+	["hi"]="hi"
 )
 
 for key in "${!tests[@]}"; do
-	output = "$(./reverse $key)"
+	output="$(./reverse $key)"
 	if ["${output}" = "${tests[$key]}"]; then
 		echo "Success test '$key'"
 	else
